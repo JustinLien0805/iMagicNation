62981d2b81d063de9fbbd2c92444647f34bc0dbb
JustinLien0805 <6998dragon@gmail.com>
2023-04-17 15:20:49 +0800

    sign in page

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,59 @@
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import axios from "axios";
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const SignInForm = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (data: FormData) => {
+    
+    // router.push("/home");
+  };
+
+  return (
+    <div className="flex w-full max-w-lg flex-col items-center justify-center space-y-4 pt-20 sm:w-3/4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full flex-col items-center justify-center space-y-4"
+      >
+        <div className="flex h-48 w-full flex-col items-center gap-4 rounded-lg bg-[#412C2B] p-8">
+          <input
+            type="email"
+            placeholder="帳號"
+            {...register("email")}
+            className="input-bordered input w-full grow border-2 border-[#1E0B12] bg-[#F6E0C1] text-[#1E0B12]"
+          />
+          <input
+            type="password"
+            placeholder="密碼"
+            {...register("password")}
+            className="input-bordered input w-full grow border-2 border-[#1E0B12] bg-[#F6E0C1] text-[#1E0B12]"
+          />
+        </div>
+        <div className="flex w-full gap-4">
+          <button
+            type="submit"
+            className="btn h-16 grow border-4 border-[#A38984] bg-[#261920] text-white"
+          >
+            登入
+          </button>
+          <button
+            type="button"
+            className="btn h-16 grow border-4 border-[#A38984] bg-[#261920] text-white"
+          >
+            註冊
+          </button>
+        </div>
+      </form>
+      <button className="w-full text-lg text-[#261920] underline underline-offset-2">
+        忘記密碼？
+      </button>
+    </div>
+  );
+};
+
+export default SignInForm;
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-
-const index = () => {
-  return <div>index</div>;
+import SignInForm from "@/components/SignInForm";
+import titleImage from "@/assets/iMagicNationIcon.png";
+import background from "@/assets/登入註冊頁面背景圖.png";
+const SignIn = () => {
+  return (
+    <div
+      className="relative flex h-screen w-screen flex-col items-center justify-center px-4"
+      style={{
+        backgroundImage: `url(${background.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <img src={titleImage.src} alt="" className="absolute top-4 scale-75" />
+      <SignInForm />
+    </div>
+  );
 };
-
-export default index;
+export default SignIn;
