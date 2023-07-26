04488b64170fb3c0ef90486a1bd57c900d3d17e2
JustinLien0805 <6998dragon@gmail.com>
2023-07-26 16:09:26 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,19 @@
 import { NextResponse } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
+import { a } from "drizzle-orm/column.d-aa4e525d";
 
 export default authMiddleware({
   publicRoutes: ["/signin"],
   async afterAuth(auth, req) {
+    const url = new URL(req.nextUrl.origin);
     if (auth.isPublicRoute) {
+      // If user is already authenticated, redirect them to the home page
+      if (auth.userId) {
+        return NextResponse.redirect(url);
+      }
       //  For public routes, we don't need to do anything
       return NextResponse.next();
     }
-
-    const url = new URL(req.nextUrl.origin);
 
     if (!auth.userId) {
       //  If user tries to access a private route without being authenticated,
