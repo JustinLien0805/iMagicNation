d2392b57e2d3fdaa67b8e85b6d37b20c79ad5e6f
JustinLien <justinlien@surveycake.com>
2023-10-14 16:00:44 +0800

    fix analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import MobileNotification from "@/components/moblie/MobileNotification";
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <div className="min-h-screen">
+      <Analytics />
       <div className="hidden sm:block">
         <ClerkProvider {...pageProps}>
           <QueryClientProvider client={queryClient}>
